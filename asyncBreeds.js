f24c9d0475f81e31d40960d2bcc0603ab33453af
Lloyd <lloyd.gerry@gmail.com>
2019-09-26 02:13:55 +0000

    adding cats

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE 1: Pass data back via callback instead of return.
+    console.log('Callback: I have the data!')
+    if (!error) callback(data); 
+  });
+}
+
+breedDetailsFromFile('Bombay', (bombay) => {
+  console.log('Return Value: ', bombay) // => print out details correctly.
+});
